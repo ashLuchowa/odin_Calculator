0d876a68375c2b861b33133e7e0dd96441270ff2
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-27 22:45:33 +0800

    Main Display + button 7

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-console.log('x:5' + ' ' + 'y:10')
+//Variables
+const mainDisplay = document.querySelector('.calc-display-main h2')
+const subDisplay = document.querySelector('.calc-display-main p')
+const buttons = document.querySelectorAll('button');
+const num7 = document.getElementById('num7');
+
+//Display
+//Main Display
+mainDisplay.textContent = '0';
+
+//Press Buttons Events
+num7.addEventListener('click', () => {
+    mainDisplay.textContent = '7';
+})
 
 
 ////
@@ -14,4 +27,3 @@ function operate(x,y,op) {
     }
 }
 
-console.log(operate(5,10,'/'))
