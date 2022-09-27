2c4cbce5fbfc58101020a9e225b1c53b696b5210
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-27 11:34:08 +0800

    Build basic operators

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log('x:5' + ' ' + 'y:10')
+
+//Add
+function addOperator(x,y) {
+    let result = x + y;
+    console.log(`Addtion: ${result}`);
+}
+addOperator(5,10);
+
+//Subtract
+function subtractOperator(x,y) {
+    let result = x - y;
+    console.log(`Subtraction: ${result}`);
+}
+subtractOperator(5,10);
+
+//Multiply
+function multiplyOperator(x,y) {
+    let result = x*y;
+    console.log(`Multiplication: ${result}`);
+}
+multiplyOperator(5,10);
+
+//Division
+function divisionOperator(x,y) {
+    let result = x/y;
+    console.log(`Division: ${result}`);
+}
+divisionOperator(5,10);
