4a2c124ce1c7bb5c727626df8f191c1261d55ca4
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-27 11:57:17 +0800

    Build operator function + add all 4 sub functions

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
 console.log('x:5' + ' ' + 'y:10')
 
-//Add
-function addOperator(x,y) {
-    let result = x + y;
-    console.log(`Addtion: ${result}`);
-}
-addOperator(5,10);
 
-//Subtract
-function subtractOperator(x,y) {
-    let result = x - y;
-    console.log(`Subtraction: ${result}`);
+////
+function operate(x,y,op) {
+    if (op === '+') {
+        return x + y;
+    } else if(op === '-') {
+        return x - y;
+    } else if(op === '*') {
+        return x * y;
+    } else if(op === '/') {
+        return x / y;
+    }
 }
-subtractOperator(5,10);
 
-//Multiply
-function multiplyOperator(x,y) {
-    let result = x*y;
-    console.log(`Multiplication: ${result}`);
-}
-multiplyOperator(5,10);
-
-//Division
-function divisionOperator(x,y) {
-    let result = x/y;
-    console.log(`Division: ${result}`);
-}
-divisionOperator(5,10);
+console.log(operate(5,10,'/'))
