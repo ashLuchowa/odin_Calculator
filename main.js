6be49a773125de6af98c5b659ae88a162a5b8b8f
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-29 11:49:08 +0800

    Change HTML btn + basic JS

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 //Variables
 const mainDisplay = document.querySelector('.calc-display-main h2')
-const subDisplay = document.querySelector('.calc-display-main p')
+const subDisplay = document.querySelector('.calc-display-sub p')
 const buttons = document.querySelectorAll('button');
-const num7 = document.getElementById('num7');
+let currentNum = '0';
+let previousNum = '';
+let Operator = '';
 
 //Display
-//Main Display
-mainDisplay.textContent = '0';
+mainDisplay.textContent = currentNum;
+subDisplay.textContent = '';
 
-//Press Buttons Events
+//--- Press Buttons Events --- //
+//Variables
+const num7 = document.getElementById('num7');
+const num2 = document.getElementById('num2');
+const keyAdd = document.getElementById('keyAdd');
+const clearButton = document.getElementById('clr-btn');
+const equalButton = document.getElementById('equal-btn');
+//num7
 num7.addEventListener('click', () => {
     mainDisplay.textContent = '7';
-})
+});
+//num2
+num2.addEventListener('click', () => {
+    mainDisplay.textContent = '2';
+});
+//+
+keyAdd.addEventListener('click', () => {
+    subDisplay.textContent = subDisplay.textContent + mainDisplay.textContent + '+';
+});
+//Clear
+clearButton.addEventListener('click', () => {
+    subDisplay.textContent = '';
+    mainDisplay.textContent = '0';
+});
 
 
-////
-function operate(x,y,op) {
-    if (op === '+') {
-        return x + y;
-    } else if(op === '-') {
-        return x - y;
-    } else if(op === '*') {
-        return x * y;
-    } else if(op === '/') {
-        return x / y;
-    }
-}
 
+//--- Operators --- //
+//Add
