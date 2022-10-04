533128e7280ef2701993f9a62ae091d92038e33e
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-05 00:23:22 +0800

    Try again

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 const mainDisplay = document.querySelector('.calc-display-main h2')
 const subDisplay = document.querySelector('.calc-display-sub p')
 const clearButton = document.getElementById('clr-btn');
-const equalButton = document.getElementById('equal-btn');
+const operators = document.querySelectorAll('.operator');
 let firstNum = 0;
 let secondNum = 0;
-let Operator = '';
+let op = '';
 
 //Display
 mainDisplay.textContent = '0';
@@ -14,38 +14,26 @@ subDisplay.textContent = '';
 //--- Press Buttons Events --- //
 const btns = document.querySelectorAll('.btn');
 for (const btn of btns) {
-    btn.addEventListener('click', function() {
-        if(typeof this.value == 'number') {
-            mainDisplay.textContent = parseInt(this.value);
-        } else {
-            mainDisplay.textContent = this.value;
-        }
+    btn.addEventListener('click', function () {
     });
 }
-
-//Equal
-equalButton.addEventListener('click', function (firstNum, secondNum, op) {
-        //Operators
-        if (op === '+') {
-            mainDisplay.textContent = firstNum + secondNum;
-            subDisplay.textContent = `${firstNum} + ${secondNum}`;
-        } else if (op === '-') {
-            mainDisplay.textContent = firstNum - secondNum;
-            subDisplay.textContent = `${firstNum} - ${secondNum}`;
-        } else if (op === '*') {
-            mainDisplay.textContent = firstNum * secondNum;
-            subDisplay.textContent = `${firstNum} * ${secondNum}`;
-        } else if (op === '/') {
-            mainDisplay.textContent = firstNum / secondNum;
-            subDisplay.textContent = `${firstNum} / ${secondNum}`;
-        }
-    });
+//Operators
+if (op === '+') {
+    mainDisplay.textContent = firstNum + secondNum;
+    subDisplay.textContent = `${firstNum} + ${secondNum}`;
+} else if (op === '-') {
+    mainDisplay.textContent = firstNum - secondNum;
+    subDisplay.textContent = `${firstNum} - ${secondNum}`;
+} else if (op === '*') {
+    mainDisplay.textContent = firstNum * secondNum;
+    subDisplay.textContent = `${firstNum} * ${secondNum}`;
+} else if (op === '/') {
+    mainDisplay.textContent = firstNum / secondNum;
+    subDisplay.textContent = `${firstNum} / ${secondNum}`;
+}
 
 //Clear
 clearButton.addEventListener('click', () => {
     subDisplay.textContent = '';
     mainDisplay.textContent = '0';
 });
-
-
-//How do we store the old number? Prompt seems to work for now
