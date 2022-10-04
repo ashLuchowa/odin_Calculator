26867fd7ed4a90d50fb3185f16a8f2d33452f14c
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-04 22:55:59 +0800

    Sub Display textContent

--- main.js
+++ main.js
@@ -8,34 +8,41 @@ let secondNum = 0;
 let Operator = '';
 
 //Display
-mainDisplay.textContent = firstNum;
+mainDisplay.textContent = '0';
 subDisplay.textContent = '';
 
 //--- Press Buttons Events --- //
 const btns = document.querySelectorAll('.btn');
 for (const btn of btns) {
-    btn.addEventListener('click', function (firstNum, secondNum, op) {
-
-        firstNum = parseInt(this.value);
-        if (firstNum != secondNum) {
-            secondNum = parseInt(prompt('second num'));
-            op = prompt('operator?');
-            console.log(`firstNum: ${firstNum}, secondNum: ${secondNum}`);
-
-            //Operators
-            if (op === '+') {
-                mainDisplay.textContent = firstNum + secondNum;
-            } else if (op === '-') {
-                mainDisplay.textContent = firstNum - secondNum;
-            } else if (op === '*') {
-                mainDisplay.textContent = firstNum * secondNum;
-            } else if (op === '/') {
-                mainDisplay.textContent = firstNum / secondNum;
-            }
-        }
-
-    });
+    //mainDisplay.textContent = parseInt(this.value);
 }
+
+//Equal
+equalButton.addEventListener('click', function (firstNum, secondNum, op) {
+
+    firstNum = parseInt(prompt('first num'));
+    if (firstNum != secondNum) {
+        secondNum = parseInt(prompt('second num'));
+        op = prompt('operator?');
+        console.log(`${firstNum} ${op} ${secondNum}`);
+
+        //Operators
+        if (op === '+') {
+            mainDisplay.textContent = firstNum + secondNum;
+            subDisplay.textContent = `${firstNum} + ${secondNum}`;
+        } else if (op === '-') {
+            mainDisplay.textContent = firstNum - secondNum;
+            subDisplay.textContent = `${firstNum} - ${secondNum}`;
+        } else if (op === '*') {
+            mainDisplay.textContent = firstNum * secondNum;
+            subDisplay.textContent = `${firstNum} * ${secondNum}`;
+        } else if (op === '/') {
+            mainDisplay.textContent = firstNum / secondNum;
+            subDisplay.textContent = `${firstNum} / ${secondNum}`;
+        }
+    }
+
+});
 
 //Clear
 clearButton.addEventListener('click', () => {
