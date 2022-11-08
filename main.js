30760a221e0e2a81b1eb470957765661f05ab253
Ash Luchowa <ashwinluchowa@gmail.com>
2022-11-08 22:41:27 +0800

    Clean up display codes

---
+++ main.js
@@ -0,0 +1,9 @@
+//------------- Selectors -------------//
+let inputNum = '';
+const mainDisplay = document.getElementById('display-main');
+const subDisplay = document.getElementById('display-sub');
+
+
+//------------- Displays -------------//
+subDisplay.textContent = '';
+mainDisplay.textContent = '0';
