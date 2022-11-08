a21f1c8d2e024e2350b3791fef8e9b77f0f2dfa2
Ash Luchowa <ashwinluchowa@gmail.com>
2022-11-08 23:10:34 +0800

    Calculator object Structure

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ let inputNum = '';
 const mainDisplay = document.getElementById('display-main');
 const subDisplay = document.getElementById('display-sub');
 
+//------------- Calculator structure -------------//
+const calculator = {
+    firstNumber: 0,
+    operator: '',
+    secondNumber: 0,
+}
 
 //------------- Displays -------------//
 subDisplay.textContent = '';
