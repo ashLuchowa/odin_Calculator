2704ef72ec734c8f1018a3bd3e39637f5b51495d
Ash Luchowa <ashwinluchowa@gmail.com>
2022-11-08 23:20:50 +0800

    HTML and JS buttons link

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let inputNum = '';
 const mainDisplay = document.getElementById('display-main');
 const subDisplay = document.getElementById('display-sub');
+const allBtn = document.querySelectorAll('.btn');
+
 
 //------------- Calculator structure -------------//
 const calculator = {
@@ -10,6 +12,23 @@ const calculator = {
     secondNumber: 0,
 }
 
+
 //------------- Displays -------------//
 subDisplay.textContent = '';
 mainDisplay.textContent = '0';
+
+
+//------------- Press buttons Numbers -------------//
+for (const button of allBtn) {
+    button.addEventListener('click', () => {
+        if (button.className === 'btn number') {
+            inputNum = inputNum + button.textContent;
+            console.log(inputNum);
+        } else if (button.className === 'btn operator') {
+            calculator.operator = button.textContent;
+            inputNum = inputNum + calculator.operator;
+            console.log(inputNum);
+        }
+    });
+}
+
